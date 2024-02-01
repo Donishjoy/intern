b7f900b0f460b87bb1117b335c4f7520c0e67ba1
Donishjoy <114819415+Donishjoy@users.noreply.github.com>
2024-02-01 11:37:50 +0530

    'first'

--- server.js
+++ server.js
@@ -1,4 +1,15 @@
 console.log("hello world")
-console.log(global);
+//console.log(global);
 const os=require('os');
+const path=require('path')
 console.log(os.type());
+console.log(os.homedir());
+console.log(os.version());
+console.log(__dirname);
+console.log(__filename);
+
+console.log(path.dirname(__filename))
+console.log(path.basename(__filename))
+console.log(path.extname(__filename))
+
+console.log(path.parse(__filename))
